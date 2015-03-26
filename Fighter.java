eb834fec0f0cfaa39bb29e07e01dd34dfdeb8839
sfc <lzzhengyi@gmail.com>
2015-03-26 17:56:58 -0400

    two updates

---
+++ Fighter.java
@@ -0,0 +1,21 @@
+
+public class Fighter {
+
+	String name;
+	int armor, //chance to avoid damage 
+	evade, //chance to avoid damage
+	track, //reduces enemy evade
+	hull, //1-2 hull is depleted per attack
+	weapon, //reduces enemy armor
+	maneuver //chance to pursue
+	; 
+	
+	/*
+	 * Battle:
+	 * 
+	 */
+	
+	public Fighter (){
+		
+	}
+}
---
+++ Planet.java
@@ -0,0 +1,16 @@
+
+public class Planet {
+
+	String name;
+	int food, industry, population, loyalty;
+	//noble slots
+	//garrison
+	
+	/**
+	 * Need to initialize the stats of the planets
+	 */
+	public Planet (String n){
+		name=n;
+		
+	}
+}
